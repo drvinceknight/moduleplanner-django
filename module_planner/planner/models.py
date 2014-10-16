35787d0fbf9f4212c8571baee696c5ac5c05f087
Alex Carney <alex.rugby101@gmail.com>
2014-10-16 17:56:53 +0100

    Added a module class to models.py

--- module_planner/planner/models.py
+++ module_planner/planner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# The Module class
+class Module(Document):
+    user = ReferenceField(User, reverese_delete_rule=CASCADE)
+    name = StringField(max_length=200, required=True)
+    desc = StringField(required=True)
+    desc_length = IntField()
+    date_modified = DateTimeField(default=datetime.now)
+    is_published = BooleanField()
