c7830d7108398b6b7e1d1a4269b58004684c2127
Alex Carney <alex.rugby101@gmail.com>
2014-10-16 17:41:37 +0100

    Woops urls.py was in the wrong place

--- module_planner/module_planner/urls.py
+++ module_planner/module_planner/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf import settings
+from planner.views import PostListView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'module_planner.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+        url(r'^$', PostListView.as_view(), name='list'),
+        url(r'^post/', include('planner.urls'))
 )
--- module_planner/planner/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from planner.views import PostListView
-
-urlpatterns = patterns('',
-        url(r'^$', PostListView.as_view(), name='list'),
-        url(r'^post/', include('planner.urls'))
-)
