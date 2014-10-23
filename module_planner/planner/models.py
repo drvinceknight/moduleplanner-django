b5303b7b18ac17b4c706d4fb5e1a7a729621df48
Alex Carney <alex.rugby101@gmail.com>
2014-10-23 17:00:30 +0100

    I think the models.py file is ok now

--- module_planner/planner/models.py
+++ module_planner/planner/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 # The Module class
-class Module():
-    user = ReferenceField(User, reverese_delete_rule=CASCADE)
-    name = StringField(max_length=200, required=True)
-    desc = StringField(required=True)
-    desc_length = IntField()
-    date_modified = DateTimeField(default=datetime.now)
-    is_published = BooleanField()
+class Module(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=500)
+    code = models.CharField(max_length=6)
+    desc_length = models.IntegerField()
