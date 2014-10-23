81002810021b5f608cdbba6fa4a153e3e5b914d9
Alex Carney <alex.rugby101@gmail.com>
2014-10-23 17:40:55 +0100

    Added a quick test view

--- module_planner/planner/views.py
+++ module_planner/planner/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It's now %s.</body></html>" % now
+    return HttpResponse(html)
