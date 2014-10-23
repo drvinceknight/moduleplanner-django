3e90797d9a4aca37722154c5e905e25d9953be5e
Alex Carney <alex.rugby101@gmail.com>
2014-10-23 17:41:27 +0100

    So we are using that quick test view

--- module_planner/module_planner/urls.py
+++ module_planner/module_planner/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from planner.views import current_datetime
 
 urlpatterns = patterns('',
+        url(r'^$', current_datetime)
 )
