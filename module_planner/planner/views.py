be5ac9964957f530ecc71bcbdaebbd00bab1d018
jason <jas.young314@gmail.com>
2014-10-24 15:07:03 +0100

    added very basic front page

--- module_planner/planner/views.py
+++ module_planner/planner/views.py
@@ -6,4 +6,5 @@ import datetime
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It's now %s.</body></html>" % now
+    return render(request, 'planner/front_page.html')
     return HttpResponse(html)
