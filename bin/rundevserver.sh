4f08893c13d7e5ee85f7bf67c5a13885094b42bf
Alex Carney <alex.rugby101@gmail.com>
2014-10-30 15:51:31 +0000

    Added a script that should automagically startup everything needed for development

---
+++ bin/rundevserver.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Change to project root and activate the virtualenv
+echo "\t=> Activating Virtualenv"
+cd ..
+source env/bin/activate
+
+# Descend into the module_planner and start mongodb
+echo "\t=> Starting MongoDB Server"
+mongod --dbpath db --smallfiles &
+sleep 5
+
+# Start python development server
+echo "\t=> Starting Development Server"
+python manage.py runserver
