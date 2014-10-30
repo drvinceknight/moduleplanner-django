92b903d0586cef93e21150e845b94e6ddb4f1083
Alex Carney <alex.rugby101@gmail.com>
2014-10-30 15:54:53 +0000

    For some reason the cd command is ot working

--- bin/rundevserver.sh
+++ bin/rundevserver.sh
@@ -2,11 +2,12 @@
 
 # Change to project root and activate the virtualenv
 echo "\t=> Activating Virtualenv"
-cd ..
+cd ../
 source env/bin/activate
 
 # Descend into the module_planner and start mongodb
 echo "\t=> Starting MongoDB Server"
+cd module_planner/
 mongod --dbpath db --smallfiles &
 sleep 5
 
