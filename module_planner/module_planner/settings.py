d7d6a79db2361a6391ad942ed7516534c91096ec
Alex Carney <alex.rugby101@gmail.com>
2014-10-30 19:57:45 +0000

    Cleanup settings.py ready for an attempt at getting testing.py to work properly

--- module_planner/module_planner/settings.py
+++ module_planner/module_planner/settings.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -26,17 +27,12 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'rest_framework',
-#    'django.contrib.staticfiles',
     'planner',
     'django_extensions'
 )
@@ -60,29 +56,8 @@ ROOT_URLCONF = 'module_planner.urls'
 
 WSGI_APPLICATION = 'module_planner.wsgi.application'
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, '..', 'media')
-STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static')
-TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, '..', 'templates')
-
-MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
-TEMPLATE_URL = '/templates/'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-
-AUTHENTICATION_BACKENDS = (
-        'mongoengine.django.auth.MongoEngineBackend',
-)
-
-SESSION_ENGINE = 'mongoengine.django.sessions'
-MONGO_DATABASE_NAME = 'plannerDB'
-
-from mongoengine import connect
-connect(MONGO_DATABASE_NAME)
+# Database - since we are using mongo we dont need to define anything here
+DATABASES = {}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -96,9 +71,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-MEDIA_URL = ''
-STATIC_URL = ''
