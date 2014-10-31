220988cdf86d47f910f2c1b02503cb0062c85a3e
Alex Carney <alex.rugby101@gmail.com>
2014-10-31 08:34:18 +0000

    Fixed indentation issues in the echo commands of the script

--- bin/rundevserver.sh
+++ bin/rundevserver.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Change to project root and activate the virtualenv
-echo "\t=> Activating Virtualenv"
+echo "      => Activating Virtualenv"
 cd ../
 source env/bin/activate
 
 # Descend into the module_planner and start mongodb
-echo "\t=> Starting MongoDB Server"
+echo "      => Starting MongoDB Server"
 cd module_planner/
 mongod --dbpath db --smallfiles &
 sleep 5
 
 # Start python development server
-echo "\t=> Starting Development Server"
+echo "      => Starting Development Server"
 python manage.py runserver
